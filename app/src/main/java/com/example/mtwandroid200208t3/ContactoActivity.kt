1ce09767b2180d8e12bdb5d593b93b0ec134dfe9
Ramón Baeza Yépez <by_ramon@outlook.com>
2020-02-11 19:03:59 -0600

    activity contactos

--- app/src/main/java/com/example/mtwandroid200208t3/ContactoActivity.kt
+++ app/src/main/java/com/example/mtwandroid200208t3/ContactoActivity.kt
@@ -2,11 +2,20 @@ package com.example.mtwandroid200208t3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_contacto.*
 
 class ContactoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacto)
+
+        txtNombreContacto.text = "Rosa Pérez"
+        txtOcupacion.text = "Ocupación: Secretaria"
+        txtTrabaja.text = "Trabaja en: SHCP"
+        txtDomicilio.text = "Domicilio: Ocampo No. 234"
+        txtCiudad.text = "Ciudad: Celaya, Gto."
+        txtCorreo.text = "Correo: rosa@gmail.com"
+        txtTelefono.text = "Tel: 4611238907"
     }
 }
--- app/src/main/java/com/example/mtwandroid200208t3/MainActivity.kt
+++ app/src/main/java/com/example/mtwandroid200208t3/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.mtwandroid200208t3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "com.example.mtwandroid200208t3.contactos.contacto"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnRosa.setOnClickListener(View.OnClickListener {
+            val intent = Intent( this, ContactoActivity::class.java)
+            intent.putExtra(TAG, "Rosa")
+            startActivity(intent)
+        })
+        btnJulian.setOnClickListener(View.OnClickListener {
+            val intent = Intent( this, ContactoActivity::class.java)
+            intent.putExtra(TAG, "Julian")
+            startActivity(intent)
+        })
+        btnSusy.setOnClickListener(View.OnClickListener {
+            val intent = Intent( this, ContactoActivity::class.java)
+            intent.putExtra(TAG, "Susy")
+            startActivity(intent)
+        })
     }
 }
